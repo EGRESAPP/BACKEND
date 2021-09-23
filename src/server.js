176e4a626fe0e63de1d4c117a01d10ce51766d00
John Doe <morganfrezze22@gmail.com>
2021-09-23 01:00:04 -0500

    application finish

--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const server = express();
 const cors = require('cors');
 const usersRouter = require('./routers/users')
 const authRouter = require("./routers/auth");
+const applicationsRouter = require('./routers/applications');
 
 //middleware
 server.use(cors())
@@ -12,6 +13,7 @@ server.use(express.json());
 //routers
 server.use("/users",usersRouter);
 server.use("/auth",authRouter);
+server.use("/applications",applicationsRouter);
 
 
 //exportar server
